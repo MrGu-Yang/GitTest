031702c9c6ce7eeddf37975b096f4787400a4063
Jeff <1875138185@qq.com>
2022-05-13 16:00:03 +0800

    push commit

--- src/main/java/com/gy/GitTest/Controller/GitTest.java
+++ src/main/java/com/gy/GitTest/Controller/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println( "master.git!");
        System.out.println( "hello.git1!");
         System.out.println( "hello.git2!");
+        System.out.println( "hello.git3!");
     }
 }
