6ffc7f414f1a039a32da2b1155d97ff945244e3e
Jeff <1875138185@qq.com>
2022-05-13 15:30:41 +0800

    idea commit

---
+++ src/main/java/com/gy/GitTest/Controller/GitTest.java
@@ -0,0 +1,14 @@
+package com.gy.GitTest.Controller;
+
+/**
+ * 需求:
+ *
+ * @Author 顾杨
+ * @date 2022年05月13日15:27
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+       System.out.println( "hello.get!");
+    }
+}
